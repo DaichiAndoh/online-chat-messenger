0295067f2803f55e66247b9a958afddf33bd5b55
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-03-22 23:28:49 +0900

    update server program

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from datetime import datetime, timedelta
 
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 9999
@@ -20,21 +21,20 @@ class ChatServer:
         self.relay_message(data, client_address)
 
     def relay_message(self, data, sender_address):
+        standard_date = datetime.now() - timedelta(seconds=30)
+
         for address in self.clients:
-            if address != sender_address:
+            if address != sender_address and self.clients[address] > standard_date:
                 self.server_socket.sendto(data, address)
 
     def run(self):
         while True:
             data, client_address = self.server_socket.recvfrom(BUFFER_SIZE)
             if client_address not in self.clients:
-                print(f"[SERVER] New client connected: {client_address}\n")
-                self.clients[client_address] = threading.Thread(
-                    target=self.handle_client, args=(data, client_address)
-                )
-                self.clients[client_address].start()
-            else:
-                self.handle_client(data, client_address)
+                print(f"[SERVER] New client connected: {client_address}")
+
+            self.clients[client_address] = datetime.now()
+            threading.Thread(target=self.handle_client, args=(data, client_address)).start()
 
 if __name__ == "__main__":
     chat_server = ChatServer()
